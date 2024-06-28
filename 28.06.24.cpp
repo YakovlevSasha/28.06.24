a378a14a8dde8ae4ed31949ce0d5ceda3b0797a6
Admin <yakovlevsasha14@gmail.com>
2024-06-29 00:03:39 +0300

    Додав 2 завдання

--- 28.06.24.cpp
+++ 28.06.24.cpp
@@ -5,6 +5,7 @@
 #include <string>
 #include <algorithm>
 #include <windows.h>
+#include <unordered_map>
 
 using namespace std;
 
@@ -88,6 +89,92 @@ using namespace std;
 
 #pragma region 2завдання 
 
+unordered_map<char, string> createTransliterationMap() {
+    unordered_map<char, string> transliterationMap;
+    transliterationMap['А'] = "A";  transliterationMap['а'] = "a";
+    transliterationMap['Б'] = "B";  transliterationMap['б'] = "b";
+    transliterationMap['В'] = "V";  transliterationMap['в'] = "v";
+    transliterationMap['Г'] = "H";  transliterationMap['г'] = "h";
+    transliterationMap['Ґ'] = "G";  transliterationMap['ґ'] = "g";
+    transliterationMap['Д'] = "D";  transliterationMap['д'] = "d";
+    transliterationMap['Е'] = "E";  transliterationMap['е'] = "e";
+    transliterationMap['Є'] = "Ye"; transliterationMap['є'] = "ie";
+    transliterationMap['Ж'] = "Zh"; transliterationMap['ж'] = "zh";
+    transliterationMap['З'] = "Z";  transliterationMap['з'] = "z";
+    transliterationMap['И'] = "Y";  transliterationMap['и'] = "y";
+    transliterationMap['І'] = "I";  transliterationMap['і'] = "i";
+    transliterationMap['Ї'] = "Yi"; transliterationMap['ї'] = "i";
+    transliterationMap['Й'] = "Y";  transliterationMap['й'] = "i";
+    transliterationMap['К'] = "K";  transliterationMap['к'] = "k";
+    transliterationMap['Л'] = "L";  transliterationMap['л'] = "l";
+    transliterationMap['М'] = "M";  transliterationMap['м'] = "m";
+    transliterationMap['Н'] = "N";  transliterationMap['н'] = "n";
+    transliterationMap['О'] = "O";  transliterationMap['о'] = "o";
+    transliterationMap['П'] = "P";  transliterationMap['п'] = "p";
+    transliterationMap['Р'] = "R";  transliterationMap['р'] = "r";
+    transliterationMap['С'] = "S";  transliterationMap['с'] = "s";
+    transliterationMap['Т'] = "T";  transliterationMap['т'] = "t";
+    transliterationMap['У'] = "U";  transliterationMap['у'] = "u";
+    transliterationMap['Ф'] = "F";  transliterationMap['ф'] = "f";
+    transliterationMap['Х'] = "Kh"; transliterationMap['х'] = "kh";
+    transliterationMap['Ц'] = "Ts"; transliterationMap['ц'] = "ts";
+    transliterationMap['Ч'] = "Ch"; transliterationMap['ч'] = "ch";
+    transliterationMap['Ш'] = "Sh"; transliterationMap['ш'] = "sh";
+    transliterationMap['Щ'] = "Shch"; transliterationMap['щ'] = "shch";
+    transliterationMap['Ю'] = "Yu"; transliterationMap['ю'] = "iu";
+    transliterationMap['Я'] = "Ya"; transliterationMap['я'] = "ia";
+    transliterationMap['ь'] = ""; 
+    transliterationMap['’'] = ""; 
+    return transliterationMap;
+}
+
+string transliterate(const string& text, const unordered_map<char, string>& transliterationMap) {
+    string result;
+    for (char c : text) {
+        if (transliterationMap.find(c) != transliterationMap.end()) {
+            result += transliterationMap.at(c);
+        }
+        else {
+            result += c;
+        }
+    }
+    return result;
+}
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    string inputFilename = "input.txt";
+    string outputFilename = "output.txt";
+
+    unordered_map<char, string> transliterationMap = createTransliterationMap();
+
+    ifstream inputFile(inputFilename);
+    if (!inputFile.is_open()) {
+        cerr << "Не вдалося відкрити файл: " << inputFilename << endl;
+        return 1;
+    }
+
+    ofstream outputFile(outputFilename);
+    if (!outputFile.is_open()) {
+        cerr << "Не вдалося відкрити файл: " << outputFilename << endl;
+        return 1;
+    }
+
+    string line;
+    while (getline(inputFile, line)) {
+        string transliteratedLine = transliterate(line, transliterationMap);
+        outputFile << transliteratedLine << endl;
+    }
+
+    inputFile.close();
+    outputFile.close();
+
+    cout << "Транслітерація завершена. Перевірте вихідний файл: " << outputFilename << endl;
+
+    return 0;
+}
 
 
 #pragma endregion
